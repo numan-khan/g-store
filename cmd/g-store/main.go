f58a2079df4b6770a1418c45f86f766bb3fadce2
Numan Hanif <numan313@gmail.com>
2022-05-06 10:43:18 +0800

    Last Commit

--- cmd/g-store/main.go
+++ cmd/g-store/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
 	api_handlers "github.com/numan-khan/g-store/internal/app/g-store/api-handlers"
+
+	"github.com/numan-khan/g-store/internal/pkg/models"
 	"github.com/numan-khan/g-store/internal/pkg/store"
+	"github.com/spf13/viper"
 )
 
 var (
 	dbLocation = flag.String("db-location", "", "Path to the database")
 	httpAddr   = flag.String("http-addr", "127.0.0.1:8080", "HTTP SERVER ADDRESS")
+	shard      = "shard1"
 )
 
 func parseCmdArgs() {
@@ -21,11 +27,45 @@ func parseCmdArgs() {
 	}
 }
 
+func parseConfig() models.Configuration {
+
+	// Set the file name of the configurations file
+	viper.SetConfigName("config")
+
+	// Set the path to look for the configurations file
+	viper.AddConfigPath("../../configs")
+
+	// Enable VIPER to read Environment Variables
+	viper.AutomaticEnv()
+
+	viper.SetConfigType("yml")
+	var configuration models.Configuration
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Error reading config file, %s", err)
+	}
+
+	err := viper.Unmarshal(&configuration)
+	if err != nil {
+		fmt.Printf("Unable to decode into struct, %v", err)
+	}
+
+	return configuration
+}
+
 func main() {
 
 	parseCmdArgs()
 
-	db, err := store.NewStore(*dbLocation)
+	config := parseConfig()
+
+	myShard, err := config.GetShardByName(shard)
+
+	if err != nil {
+		errors.New("Error loadig configuration ...")
+	}
+
+	db, err := store.NewStore(*dbLocation, myShard, len(config.Shards) )
 
 	if err != nil {
 		log.Fatal(err)
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/boltdb/bolt v1.3.1
+	github.com/spf13/viper v1.10.1
 	golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27 // indirect
 )
